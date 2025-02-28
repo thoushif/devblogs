57339f5e2775f3b643a16a14eb0281f3dd0e7368
Thoushif Aazam Shaik <aazam555@gmail.com>
2025-02-28 15:13:23 +0000

    fixed build issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
 import "../styles/global.css";
-import Clarity from '@microsoft/clarity';
-const projectId = process.env.CLARITY_PROJECT_ID
 
 function MyApp({ Component, pageProps }) {
-  Clarity.init(projectId);
+ 
   return (
     <Layout>
       <Head>
